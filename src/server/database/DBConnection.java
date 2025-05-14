8702d5bc9812bb94c39d873f35dabc69518492bd
Jorge Frederic Terre <2240705@slu.edu.ph>
2025-05-14 21:22:15 +0800

    test

--- src/server/database/DBConnection.java
+++ src/server/database/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    public static Connection con;
+    static Connection con;
 
     private DBConnection(){}
 
@@ -13,7 +13,7 @@ public class DBConnection {
         try {
             String url = "jdbc:mysql://localhost:3306/wordwarz";
             String user = "root";
-            String password = "francescute";
+            String password = "root";
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             con = DriverManager.getConnection(url, user, password);
